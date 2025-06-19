f184792e476e487116b72a61e042d12eca4f6d56
seb <138612354+vespoli-s@users.noreply.github.com>
2025-06-19 08:42:49 -0400

    Update SummaryScreen.jsx

--- src/screens/SummaryScreen.jsx
+++ src/screens/SummaryScreen.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SummaryScreen = () => {
   return (
     <div className="onboarding-screen">
       <h2>You're All Set!</h2>
       <p>Your budget profile has been created.</p>
-      <p>From here, you’ll be able to enter new paychecks, track spending, and get 50/30/20 insights.</p>
-
-      <button onClick={() => alert('Dashboard coming soon!')}>Go to Dashboard</button>
+      <Link to="/dashboard">
+        <button>Go to Dashboard</button>
+      </Link>
     </div>
   );
 };
